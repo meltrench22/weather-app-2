a467337004e53d33eb2766b9477d538d3c485efd
meltrench22 <amandatrenchfield@icloud.com>
2025-04-25 03:07:28 -0500

    updated my javascript and html

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-let apiKey = "t1a14ccfc72cfoaaeca2b9b3f85290df";
+let apiKey = "t1a14ccfc72cfoaaeca2b9b3f85290df"; // Your real API key
 
-let searchInput = document.querySelector(".search input");
-let searchButton = document.querySelector(".search nutton");
+window.addEventListener("DOMContentLoaded", function () {
+  let searchInput = document.querySelector(".search input");
+  let searchButton = document.querySelector(".search button");
 
-async function getWeather(city) {
-  let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apikey}`
-  );
+  searchButton.addEventListener("click", function () {
+    let city = searchInput.value;
+    if (city) {
+      getWeather(city);
+    }
+  });
 
-  if (response.ok) {
-    let data = await response.json();
-    updateWeatherCard(data);
-  } else {
-    alert("City not found");
+  async function getWeather(city) {
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    );
+
+    if (response.ok) {
+      let data = await response.json();
+      updateWeatherCard(data);
+    } else {
+      alert("City not found!");
+    }
   }
-}
 
-function updateWeatherCard(data) {
-  document.querySelector(".city").textContent = data.name;
-  document.querySelector(".temperature").textContent = `${Math.round(
-    data.main.temp
-  )}°C`;
-  document.querySelector(
-    ".humidity span"
-  ).textContent = `${data.main.humidity}%`;
-  document.querySelector(".wind span").textContent = `${data.wind.speed}km/h`;
+  function updateWeatherCard(data) {
+    document.querySelector(".city").textContent = data.name;
+    document.querySelector(".temperature").textContent = `${Math.round(
+      data.main.temp
+    )}°C`;
+    document.querySelector(
+      ".humidity span"
+    ).textContent = `${data.main.humidity}%`;
+    document.querySelector(
+      ".wind span"
+    ).textContent = `${data.wind.speed} km/h`;
 
-  let iconCode = data.weather[0].icon;
-  document.querySelector(
-    ".weather-icon"
-  ).src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-}
-
-searchButton.addEventListener("click", function () {
-  let city = searchInput.value;
-  if (city) {
-    getWeather(city);
+    let iconCode = data.weather[0].icon;
+    document.querySelector(
+      ".weather-icon"
+    ).src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
   }
 });
